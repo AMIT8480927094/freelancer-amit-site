f6f3e2ebac4bcf521b44fec10dcb7da7e11ea592
AMIT8480927094 <sibunu753788@gmail.com>
2025-07-16 01:44:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,5 +89,9 @@ def service_detail(name):
     else:
         return "Service not found", 404
 
+import os
+
 if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
